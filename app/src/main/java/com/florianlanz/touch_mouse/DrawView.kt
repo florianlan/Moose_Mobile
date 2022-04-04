330cbe311d7d94cd264bcf0f3bd5020eaa9631e4
Florian Lanz <florianlanz73@gmail.com>
2022-04-04 11:14:17 +0200

    implemented function to choose via 3 buttons on MainActivity the size of the grid

--- app/src/main/java/com/florianlanz/touch_mouse/DrawView.kt
+++ app/src/main/java/com/florianlanz/touch_mouse/DrawView.kt
@@ -4,22 +4,46 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.util.AttributeSet
+import android.view.KeyEvent
 import android.view.View
+import android.widget.TextView
 
 class DrawView(ctx: Context) : View(ctx) {
+    var size = 2
     var paint = Paint()
+
+    constructor(ctx: Context, size: Int) : this(ctx) {
+        this.size = size
+    }
 
     private fun init() {
         paint.color = Color.BLACK
     }
 
 
-
+    /**
+     * draw the grid of selected grid size
+     */
     override fun onDraw(canvas: Canvas) {
-        val width = this.width
-        canvas.drawLine(width/2f, 0f, width/2f, width.toFloat(), paint)
-        canvas.drawLine(0f, width/2f, width.toFloat(), width/2f, paint)
+        val width = this.width / size
+        for (i in 1 until size) {
+            //vertical lines
+            canvas.drawLine(
+                i * width.toFloat(),
+                0f,
+                i * width.toFloat(),
+                size * width.toFloat(),
+                paint
+            )
+            //horizontal lines
+            canvas.drawLine(
+                0f,
+                i * width.toFloat(),
+                size * width.toFloat(),
+                i * width.toFloat(),
+                paint
+            )
+        }
     }
 
 }
--- app/src/main/java/com/florianlanz/touch_mouse/MainActivity.kt
+++ app/src/main/java/com/florianlanz/touch_mouse/MainActivity.kt
@@ -3,18 +3,33 @@ package com.florianlanz.touch_mouse
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
-    lateinit var drawView: DrawView
+    private lateinit var drawView: DrawView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        drawView = DrawView(this)
+    }
+
+    fun create2x2Grid(view: View) {
+        drawView = DrawView(this, 2)
         drawView.setBackgroundColor(Color.WHITE)
         setContentView(drawView)
+    }
 
+    fun create3x3Grid(view: View) {
+        drawView = DrawView(this, 3)
+        drawView.setBackgroundColor(Color.WHITE)
+        setContentView(drawView)
+    }
+
+    fun create4x4Grid(view: View) {
+        drawView = DrawView(this, 4)
+        drawView.setBackgroundColor(Color.WHITE)
+        setContentView(drawView)
     }
 
 
